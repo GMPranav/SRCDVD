483a59b178b1b9fba92c452b1af9758ac8ce1970
partoftheworlD <partoftheworldcs@gmail.com>
2021-07-25 12:29:57 +0300

    Twitch links getting detected now

--- Twitch.py
+++ Twitch.py
@@ -11,6 +11,9 @@ class TwitchAPI:
 		}
 		self.twitchAuthAPIKey = ""
 		self.header = {}
+		self.videoIDArray = []
+		self.offset = 100
+		self.notPrivate = []
 
 	def auth(self):
 		self.body['client_id'] = input("Enter Client-ID: ")
@@ -25,20 +28,27 @@ class TwitchAPI:
 		if response.status_code == 400:
 			print("[SKIP] Some of client keys not valid. Please pass a valid client key.")
 			return False
+	
+	def getVideoID(self, videoLink):
+		id = re.compile(r'(\d{8,11}).*')
+		videoID = re.findall(id, videoLink)
+		if videoID:
+			self.videoIDArray.append(videoID.pop())
 
-	def checkVideo(self, videoLink):
+	def checkVideo(self):
 		try:
-			id = re.compile(r'\d{8,11}')
-			videoID = re.findall(id, videoLink)
-			if videoID:
-				url = "https://api.twitch.tv/helix/videos?id="+videoID.pop()
+			for offset in range(0, len(self.videoIDArray)+self.offset, self.offset):
+				ids = ','.join([str(x) for x in self.videoIDArray[offset:self.offset+offset]])
+				url = "https://api.twitch.tv/helix/videos?id=" + ids
 				response = requests.get(url, headers=self.header)
 				if response.status_code == 200:
 					# Return link if it dead
-					if response.json()['status'] == 404:
-						return videoLink
-		# KeyError occurs if a json object without a 'status' key
-		# is received in response from the server 
-		# In principle, if response code is 200, this should not happen
+					for i in response.json()['data']:
+						self.notPrivate.append(i['id'])
+
+			return (set(self.videoIDArray)-set(self.notPrivate))
+			# KeyError occurs if a json object without a 'status' key
+			# is received in response from the server 
+			# In principle, if response code is 200, this should not happen
 		except KeyError:
 			pass
